1ee1b8a308f42c121a1e5c2cc3e94225d4f1e0d2
Jaehoon Hwang <jaehoon217@gmail.com>
2023-11-25 23:48:18 -0800

    setup(tsnd/eslint/tsconfig): apparently setting up absolute import needs lots of dependencies

---
+++ src/days/1/index.ts
@@ -0,0 +1,1 @@
+export const name = "day 1; from index.ts";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-console.log("HELLO");
-console.log("WORLD");
+import {name} from "@aoc/days/1/index";
+
+console.log("Hello, World!");
+console.log(name);
+console.log("HI");
+
