7c8dfcfc0b210b7ab4f15fa83f5dd7e2c5941898
Jaehoon Hwang <jaehoon217@gmail.com>
2023-12-11 00:56:54 -0800

    (misc): adding templates.
    Cause it's boring as heck to repeat this, just copy the files.

---
+++ src/days/template/index.ts
@@ -0,0 +1,19 @@
+import { ReadFileSample, ReadFileProblem } from "@aoc/utils";
+
+export const name = "day _N_; from index.ts";
+export const day = "_N_";
+
+const problemInputs = ReadFileProblem(day);
+const sampleInputs = ReadFileSample(day);
+
+export function SolveDay_N_() {
+  console.log("it's day _N_");
+
+  solvePartOne(problemInputs); // Solved it.
+  solvePartTwo(sampleInputs); // Solved it.
+}
+
+function solvePartOne(inputs: string[]) {}
+
+function solvePartTwo(inputs: string[]) {}
+
