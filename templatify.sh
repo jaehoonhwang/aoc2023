fd9d27cf097e1a2ffcc24ec4df4cd38cb13fa467
Jaehoon Hwang <jaehoon217@gmail.com>
2023-12-11 01:09:25 -0800

    (misc): move on template file.

--- templatify.sh
+++ templatify.sh
@@ -6,6 +6,6 @@ day=2
 dayName="Two"
 
 mkdir -p src/days/$day
-cp -r src/days/template/. src/days/$day/
+cp -r src/template/. src/days/$day/
 sed -i -e "s/_N_/$day/g" src/days/$day/index.ts
 sed -i -e "s/_NAME_/$dayName/g" src/days/$day/index.ts
